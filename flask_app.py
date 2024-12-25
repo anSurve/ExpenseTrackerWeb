f7b2f6c843ff1766f38ce66ab13989e26fe21968
aniket.surve <aniket.surve@synechron.com>
2024-12-25 14:35:00 +0530

    Commit for website

--- flask_app.py
+++ flask_app.py
@@ -4,9 +4,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 #CORS(app)
 
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/apps')
+def apps():
+    return render_template('apps.html')
+
+
+@app.route('/expense_tracker')
+def expense_tracker():
+    return render_template('expense_tracker.html')
+
 
 if __name__ == '__main__':
     app.run()
